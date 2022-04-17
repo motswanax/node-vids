a509d312509eba6136590638e68855f43da2e5ef
motswanax <motswanax@users.noreply.github.com>
2022-04-17 13:27:23 +0200

    Added register user endpoint and model.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const movies = require("./routes/movies");
 const rentals = require("./routes/rentals");
+const users = require("./routes/users");
 
 mongoose
   .connect("mongodb://localhost/vidly")
@@ -20,6 +21,7 @@ app.use("/api/genres", genres);
 app.use("/api/customers", customers);
 app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
+app.use("/api/users", users);
 
 // Event listener for HTTP requests.
 const port = process.env.PORT || 3000;

b4027113f729df3815d71517ba27f9e654e6b071
motswanax <motswanax@users.noreply.github.com>
2022-04-18 16:38:23 +0200

    Added JWT and stored secret key in environment variables.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
@@ -7,6 +8,12 @@ const customers = require("./routes/customers");
 const movies = require("./routes/movies");
 const rentals = require("./routes/rentals");
 const users = require("./routes/users");
+const auth = require("./routes/auth");
+
+if (!config.get("jwtPrivateKey")) {
+  console.error("FATAL ERROR: jwtPrivateKey is not defined.");
+  process.exit(1);
+}
 
 mongoose
   .connect("mongodb://localhost/vidly")
@@ -22,6 +29,7 @@ app.use("/api/customers", customers);
 app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
 app.use("/api/users", users);
+app.use("/api/auth", auth);
 
 // Event listener for HTTP requests.
 const port = process.env.PORT || 3000;

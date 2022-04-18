cba377882934536ff280c050f1f291c6a5667429
motswanax <motswanax@users.noreply.github.com>
2022-04-18 16:44:02 +0200

    Added token to response header.

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
+const config = require("config");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const _ = require("lodash");
 const { User, validate } = require("../models/user");
@@ -18,7 +20,11 @@ router.post("/", async (req, res) => {
 
   await user.save();
 
-  res.send(_.pick(user, ["_id", "name", "email"]));
+  const token = jwt.sign({ _id: user._id }, config.get("jwtPrivateKey"));
+
+  res
+    .header("x-auth-token", token)
+    .send(_.pick(user, ["_id", "name", "email"]));
 });
 
 // Get all.
